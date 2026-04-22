b28d66c834a347edd03e7cc6de407311b38cad76
salma-elanigri <salmaelanigri@gmail.com>
2026-04-22 22:09:55 +0100

    use streaming api

--- src/infrastructure/anthropic_service.py
+++ src/infrastructure/anthropic_service.py
@@ -7,18 +7,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class AnthropicService(ILLMService):
-    def __init__(self, model_name: str = "claude-haiku-4-5", max_tokens: int = 1000):
+    def __init__(self, model_name: str = "claude-haiku-4-5-20251001", max_tokens: int = 1000):
         self.model_name = model_name
         self.client = anthropic.Anthropic()
         self.max_tokens = max_tokens
 
     def generate(self, prompt: str) -> Generator:
-        response = self.client.messages.create(
+        with self.client.messages.stream(
             model=self.model_name,
             max_tokens=self.max_tokens,
-            messages=[ {'role':'user',
-                'content':prompt}],
-        )
-        # Extract the text content from the response
-        for chunk in response:
-            yield chunk.content
+            messages=[{"role": "user", "content": prompt}],
+        ) as stream:
+            for text in stream.text_stream:
+                yield text
