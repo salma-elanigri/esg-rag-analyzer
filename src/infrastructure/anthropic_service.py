b6d02a1e67e7976c270c0e6f5f61a8afe9059f8f
salma-elanigri <salmaelanigri@gmail.com>
2026-04-23 15:21:47 +0100

    use langchain anthropic adaptation instead of anthropic api

--- src/infrastructure/anthropic_service.py
+++ src/infrastructure/anthropic_service.py
@@ -1,22 +1,18 @@
-import anthropic
+from langchain_anthropic import ChatAnthropic
 
 from src.core.interfaces import ILLMService
 from typing import Generator
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 class AnthropicService(ILLMService):
     def __init__(self, model_name: str = "claude-haiku-4-5-20251001", max_tokens: int = 1000):
         self.model_name = model_name
-        self.client = anthropic.Anthropic()
         self.max_tokens = max_tokens
 
     def generate(self, prompt: str) -> Generator:
-        with self.client.messages.stream(
-            model=self.model_name,
-            max_tokens=self.max_tokens,
-            messages=[{"role": "user", "content": prompt}],
-        ) as stream:
-            for text in stream.text_stream:
-                yield text
+        llm = ChatAnthropic(model=self.model_name,
+                            max_tokens=self.max_tokens,)
+        for chunk in llm.stream(prompt):
+            yield chunk.text
